05827807c651610560adeb67da5fbc4203c4fb08
Hazealnut <hazealnut7@gmail.com>
2024-08-14 15:02:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+document.querySelector('body').style.backgroundColor="beige";
+document.querySelector('body').style.display="flex";
+document.querySelector('body').style.flexDirection="column";
+document.querySelector('body').style.maxHeight="100vh";
+document.querySelector('body').style.justifyContent="center";
+document.querySelector('body').style.alignItems="left";
+
+let collab = document.getElementById('collab');
+collab.innerHTML="Collaborating <br> With Hazelnut";
+collab.style.color="dark brown";
+collab.style.fontSize="4em";
+collab.style.textAlign="left";
+collab.style.fontFamily="Poppins";
+collab.style.fontWeight="bold";
+collab.style.marginTop="1.9em";
+collab.style.marginLeft="1em";
+
+let desc = document.getElementById('desc');
+desc.innerHTML="I am a student passionated with UI and UX Design <br> Follow and hire me for more information";
+desc.style.color="dark brown";
+desc.style.fontSize="1.4rem";
+desc.style.textAlign="left";
+desc.style.fontFamily="Poppins";
+desc.style.fontWeight="bold";
+desc.style.marginLeft="3em";
+desc.style.marginTop="1.7em";
+
+let port = document.getElementById('port');
+port.innerHTML="Portofolio";
+port.style.color="dark brown";
+port.style.fontSize="1.2rem";
+port.style.fontFamily="Poppins";
+port.style.fontWeight="bold";
+port.style.marginLeft="60em";
+port.style.marginTop="-20.5em";
+
+let contact = document.getElementById('contact');
+contact.innerHTML="Contacts";
+contact.style.color="dark brown";
+contact.style.fontSize="1.2rem";
+contact.style.fontFamily="Poppins";
+contact.style.fontWeight="bold";
+contact.style.marginLeft="54em";
+contact.style.marginTop="-1.5em";
+
+let about = document.getElementById('about');
+about.innerHTML="About";
+about.style.color="dark brown";
+about.style.fontSize="1.2rem";
+about.style.fontFamily="Poppins";
+about.style.fontWeight="bold";
+about.style.marginLeft="49.1em";
+about.style.marginTop="-1.5em";
+
+let button = document.getElementById('button');
+button.innerHTML="Hire Me"
+button.style.color="dark brown";
+button.style.fontSize="1.2rem";
+button.style.fontFamily="Poppins";
+button.style.fontWeight="bold";
+button.style.marginLeft="3.5em";
+button.style.marginTop="21.7em";
+button.style.padding="1em";
+button.style.backgroundColor="lightblue";
+button.style.width="6vw";
+button.style.borderRadius="0.7em";
+
+let line = document.getElementById('line');
+line.style.padding="2px";
+line.style.backgroundColor="black";
+line.style.marginTop="-28.5em";
+line.style.width="90vw";
+line.style.marginLeft="3em";
+
+let image=document.getElementById('image');
+image.style.minWidth='10em';
+image.style.minHeight='10em';
+image.style.width='20em';
+image.style.height='20em';
+image.style.marginLeft="52em";
+image.style.marginTop="5em";
+
+image.style.backgroundImage="url('https://images.pexels.com/photos/27661550/pexels-photo-27661550/free-photo-of-black-and-white-photograph-of-roses-in-a-vase.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')";
+image.style.borderRadius='50%';
